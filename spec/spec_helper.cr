5611905bdf1ce3fd0febd56d27f9ef5396c6a348
Robert Schulze <robert@dotless.de>
2022-06-15 14:18:58 +0200

    Format

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -6,11 +6,13 @@ DATA = {
   foo2: "bar2",
   foo:  ("bar-"*128),
 }
+
 def data
   DATA
 end
 
 DUMPED_WITHOUT_HEADER = File.read("spec/fixtures/test.dump").split("# End of header\n")[1]
+
 def dumped_without_header
   DUMPED_WITHOUT_HEADER
 end
